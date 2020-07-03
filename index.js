9e86d78e2746f7ddf0c28665f249380bfbd9ab1b
Lance Pollard <lp@elk.fm>
2020-07-02 17:23:00 -0700

    Another attempt

--- index.js
+++ index.js
@@ -1,44 +1,90 @@
 
-exports.getNumberOfBitsInInteger = function(int32) {
-  if (int32 === 0) return 1
-  return Math.floor(Math.log2(int32)) + 1
+exports.getNumberOfBitsInUint32 = function(uint32) {
+  let count = 0
+  while (uint32) {
+    count++
+    uint32 >>= 1
+  }
+  return count
 }
 
-exports.getRangeOfBitsFromInteger = function(int32, left, size) {
-  let total = exports.getNumberOfBitsInInteger(int32)
+exports.getRangeOfBitsFromUint32 = function(uint32, left, size) {
+  let total = exports.getNumberOfBitsInUint32(uint32)
   let right = total - left - size
   let powerOf2 = 1 << total
   let all1s = powerOf2 - 1
   let all1sOnRight = all1s >> left
   let all1sOnLeft = all1s << right
   let all1sInMiddle = all1sOnRight & all1sOnLeft
-  let matching = int32 & all1sInMiddle
+  let matching = uint32 & all1sInMiddle
   let shifted = matching >> right
   return shifted
 }
 
-exports.setBitsOnInteger = function(int32, left, value) {
-  let total = exports.getNumberOfBitsInInteger(int32)
-  let size = exports.getNumberOfBitsInInteger(value)
-  let right = total - left - size
-  let powerOf2 = 1 << total
-  let all1s = powerOf2 - 1
-  let all1sOnRight = all1s >> left
-  let all1sOnLeft = all1s << right
-  let all1sInMiddle = all1sOnRight & all1sOnLeft
-  let all1sOnBounds = ~all1sInMiddle
-  let kept = int32 & all1sOnBounds
-  let shifted = value << right
-  let combined = kept | shifted
-  return combined
+exports.setBitsOnUint32 = function(uint32, left, value) {
+  let sourceSize = exports.getNumberOfBitsInUint32(uint32)
+  let valueSize = exports.getNumberOfBitsInUint32(value)
+
+  // case 1: 101 + 101 at 3 = 101101
+  // case 2: 101 + 101 at 2 = 10101
+  // case 3: 101 + 1 at 1 = 111
+  let targetExpanse = left + valueSize
+  let targetSize = Math.max(sourceSize, targetExpanse)
+  let sourceUpdated = uint32
+
+  if (targetSize > sourceSize) {
+    let targetGrow = targetSize - sourceSize
+    // now we have the full length target
+    sourceUpdated = sourceUpdated << targetGrow
+  }
+
+  // write value at a specific position relative to the target
+  let valueGrow = targetSize - left - valueSize
+  let valueUpdated = value << valueGrow
+  let valueUpdatedSize = valueSize + valueGrow
+
+  // clear the target at the value position
+  let valuePowerOf2 = 1 << valueSize
+  let valueAll1s = valuePowerOf2 - 1
+  let valueAll1sShifted = valueAll1s << valueGrow
+  let valueAll1sShiftedInverse = ~valueAll1sShifted
+  let sourceUpdatedWithOpenSpot = valueAll1sShiftedInverse & sourceUpdated
+
+  let output = sourceUpdatedWithOpenSpot | valueUpdated
+  return output
 }
 
-exports.getRangeOfBitsFromBuffer = function(buffer, start, size) {
-  const sizeDividedBy8 = size >> 3
-  let resultByteBuffer = new Uint8Array(sizeDividedBy8)
+exports.getRangeOfBitsFromUint32Buffer = function(read32Buffer, readLeft, readSize, writeUint32Buffer, writeLeft) {
+  let i = 0
+  let power2To5 = 1 << 5
+  let power2To5All1s = power2To5 - 1
+  while (i < readSize) {
+    let readUint32Offset = readLeft >> 32
+    let uint32 = read32Buffer[readUint32Offset]
+    let readBitOffset = readLeft & power2To5All1s
+    let readRightBitSize = 32 - readBitOffset
+    let bits = exports.getRangeOfBitsFromUint32(uint32, readBitOffset, readRightBitSize)
+    let total = exports.getNumberOfBitsInUint32(bits)
+    exports.setBitsUsingUint32InUint32Buffer(writeUint32Buffer, writeLeft, bits)
+    writeLeft += total
+    readLeft += readRightBitSize
+    i += readRightBitSize
+  }
 }
 
-exports.getLastNBitsFromInteger = function(int32, n) {
+exports.setBitsUsingUint32InUint32Buffer = function(writeUint32Buffer, left, writeUint32) {
+  let readUint32Left = left >> 5
+  let readUint32 = writeUint32Buffer[readUint32Left]
+  let readBitLeft = left
+  let outUint32 = exports.setBitsOnUint32(readUint32, readBitLeft, writeUint32)
+  writeUint32Buffer[readUint32Left] = outUint32
+}
+
+exports.setBitsUsingStringInUint8Buffer = function(write8Buffer, left, string) {
+
+}
+
+exports.getLastNBitsFromUint32 = function(int32, n) {
   // all 1s for n bits
   const mask = (1 << n) - 1
   // AND it to the integer so only matching bits remain.
--- test.js
+++ test.js
@@ -7,35 +7,42 @@ const x17 = 0b10001
 const x23 = 0b10111
 const x185 = 0b10111001
 
-const a1 = bit.getRangeOfBitsFromInteger(x5, 0, 3)
-assertBits(a1, '101')
+// const a1 = bit.getRangeOfBitsFromUint32(x5, 0, 3)
+// assertBits(a1, '101')
 
-const a2 = bit.getRangeOfBitsFromInteger(x185, 2, 4)
-assertBits(a2, '1110')
+// const a2 = bit.getRangeOfBitsFromUint32(x185, 2, 4)
+// assertBits(a2, '1110')
 
-const a3 = bit.getRangeOfBitsFromInteger(x185, 0, 6)
-assertBits(a3, '101110')
+// const a3 = bit.getRangeOfBitsFromUint32(x185, 0, 6)
+// assertBits(a3, '101110')
 
-const a4 = bit.getRangeOfBitsFromInteger(x185, 1, 5)
-assertBits(a4, '1110')
+// const a4 = bit.getRangeOfBitsFromUint32(x185, 1, 5)
+// assertBits(a4, '1110')
 
-const b1 = bit.setBitsOnInteger(x185, 1, 1)
+const b1 = bit.setBitsOnUint32(x185, 1, 1)
 assertBits(b1, '11111001')
 
-const b2 = bit.setBitsOnInteger(x185, 1, 2)
+const b2 = bit.setBitsOnUint32(x185, 1, 2)
 assertBits(b2, '11011001')
 
-// const a3 = bit.getRangeOfBitsFromInteger(5, 1, 2)
-// assertBits(a3, '1')
+const b3 = bit.setBitsOnUint32(x185, 8, x5)
+assertBits(b3, '10111001101')
 
-// const a4 = bit.getRangeOfBitsFromInteger(x185, 3, 3)
-// assertBits(a4, '111')
+// const z1 = new Uint8Array(16)
+// z1[0] = x185
+// z1[1] = x185
+// z1[3] = x185
+// const z2 = new Uint8Array(16)
+// bit.getRangeOfBitsFromUint32Buffer(z1, 4, 4, z2, 0)
+// assertBuffer(z2, 0, '1001')
 
-// const a5 = bit.getRangeOfBitsFromInteger(x185, 2, 4)
-// assertBits(a5, '1110')
-
-// const a5 = bit.setBitsInInteger(x185, 3, 3)
-// assertBits(a5, '1110')
+// const z1 = new Uint32Array(16)
+// bit.setBitsUsingUint32InUint32Buffer(z1, 0, 5)
+// assertBuffer(z1, 0, '101')
+// bit.setBitsUsingUint32InUint32Buffer(z1, 2, 5)
+// assertBuffer(z1, 0, '10101')
+// bit.setBitsUsingUint32InUint32Buffer(z1, 5, 5)
+// assertBuffer(z1, 0, '10110101')
 
 console.log(`
     success
@@ -43,4 +50,8 @@ console.log(`
 
 function assertBits(a, b) {
   assert.equal(a.toString(2), b)
+}
+
+function assertBuffer(buffer, index, value) {
+  assert.equal(buffer[index].toString(2), value)
 }
